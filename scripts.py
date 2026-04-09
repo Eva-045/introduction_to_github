91ab38fa5426fdd17c31f926171e137dfcbcb86d
Eva-045 <einsteinwangari@gmail.com>
2026-04-09 20:49:51 +0300

    Adding blog.html and scripts.py,editing contents of scrips.py

---
+++ scripts.py
@@ -0,0 +1,5 @@
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et cursus quam, vitae tristique erat. Sed ut dui massa.
+ Suspendisse finibus nulla risus, sit amet pulvinar elit gravida placerat. 
+Nam sollicitudin vestibulum nunc non ultrices. 
+Aenean vehicula sapien eget nulla cursus, finibus convallis justo gravida. Nulla elit mi, bibendum ac diam ac, elementum ullamcorper libero.
+ Aenean gravida vehicula porttitor. Sed tempus eros elementum euismod convallis.
